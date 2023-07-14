17cd3d35656d822088cfc197e0050e201fea2775
Akins20 <ogunbiye@gmail.com>
2023-07-14 12:33:53 +0100

    Installed React-player

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
+import ReactPlayer from "react-player";
 
 function Playlist() {
-    const [albums, setAlbums] = React.useState([]);
+    const [album, setAlbum] = React.useState([]);
+    const playerRef = useRef(null);
+    // const spotifyUrl = album.external_urls.spotify.toUrl;
+    // <ReactPlayer ref={playerRef} url={album.external_urls.spotify} controls={true}/>
     async function loadMusic() {
         const url = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
         const options = {
@@ -15,24 +19,26 @@ function Playlist() {
         try {
             const response = await fetch(url, options);
             const result = await response.json();
+            console.log(result);
             const data = result["tracks"].pop();
             console.log(data);
-            setAlbums(data.album);
+            const dataArray = Object.values(data);
+            setAlbum(dataArray);
         } catch (error) {
             console.error(error);
         }
 
     };
-    async function viewAlbums() {
-        albums.map((albums, i) => {
-            console.log(albums)
-            return (
-                <li>
-                    <button className="btn bg-white">Put album here <span className="badge bg-light text-dark">+</span></button>
-                </li>
-            )
-        })
-    };
+    // async function viewAlbums() {
+    //     albums.map((albums, i) => {
+    //         console.log(albums)
+    //         return (
+    //             <li>
+    //                 <button className="btn bg-white">Put album here <span className="badge bg-light text-dark">+</span></button>
+    //             </li>
+    //         )
+    //     })
+    // };
     return (
         <div className="playlist">
             <h3>Trending songs</h3>
@@ -46,16 +52,20 @@ function Playlist() {
                 <li>
                     <button className="btn bg-white">Unavailable by Davido <span className="badge bg-light text-dark">+</span></button>
                 </li>
-                <li>
-                    <button className="btn bg-white">Unavailable by Davido <span className="badge bg-light text-dark">+</span></button>
-                </li>
-                <li>
-                    <button className="btn bg-white">Unavailable by Davido <span className="badge bg-light text-dark">+</span></button>
-                </li>
+                {React.Children.toArray(
+                album.map((album) => {
+                    console.log(album.external_urls)
+                    return (
+                        <li>
+                            <ReactPlayer ref={playerRef} url={album} controls={true}/>
+                            <button className="btn bg-white">{album.name}<span className="badge bg-light text-dark">+</span></button>
+                        </li>
+                    )
+                }))}
 
             </ul>
             <button className="btn btn-warning mb-3" onClick={loadMusic}>Refresh Albums</button>
-            <button className="btn btn-warning mb-3" onClick={viewAlbums}>View Albums</button>
+            {/* <button className="btn btn-warning mb-3" onClick={viewAlbums}>View Albums</button> */}
 
         </div>
     )
